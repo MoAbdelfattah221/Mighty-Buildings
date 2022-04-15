9a068d1e38c54169e242412a1d76858cd25db8a0
Mohamed-Abdelfattah <moabdelfattah221@gmail.com>
2022-04-15 03:31:06 +0200

    Mighty Buildings Commit

---
+++ javascript/mighty.js
@@ -0,0 +1,5 @@
+let menu = document.querySelector(".menu");
+    menu.addEventListener("click",function(){
+        let navList = document.querySelector(".nav-list");
+        navList.classList.toggle("active");
+    });
